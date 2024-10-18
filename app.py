34d8f81c573f7f2b6653084376adcaf936177b3d
root <root@BABA-TOHEROU>
2024-10-18 16:32:41 +0200

    My first Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+
+st.write("Hello World")
+st.title("Data Visualization web application")
+
+st.header("Part 1: Data Exploration")
+st.write("In this section, we will explore the Altair cars dataset.")
+st.markdown("*Further resources [here](https://altair-viz.github.io/gallery/selection_histogram.html)*")
+
+slider = st.slider("Slider title", 0, 100, 50)
+check = st.checkbox("Checkbox title", ["Add a constant", "Add beta 1", "Add beta 2"])
+radio = st.radio("Radio title", ["Yes", "No"])
+txt = st.text_input("Type here")
+txt_area = st.text_area("Type here")
+button = st.button("Button name")
+
+
+if st.button("Click to launch"):
+    button
+
+st.snow()
+
+st.balloons()
